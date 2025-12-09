9331174370ee4bf773e0b02a185ff3ee83c51387
Lentian <lentianlatifi@gmail.com>
2025-12-09 21:24:05 +0100

    Remove the custom pricing for image, since we don't use anymore dalle-3.

--- litellm/cost_calculator.py
+++ litellm/cost_calculator.py
@@ -1570,11 +1570,6 @@ def default_image_cost_calculator(
         raise Exception(
             f"Model not found in cost map. Tried checking {models_to_check}"
         )
-
-    # zalazium fix (custom pricing)
-    pixel_price_euro = (3.3531189e-8) * 7
-    cost_info["input_cost_per_pixel"] = pixel_price_euro
-    
     return cost_info["input_cost_per_pixel"] * height * width * n
 
 
